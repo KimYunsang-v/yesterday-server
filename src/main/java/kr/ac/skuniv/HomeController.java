bfdb99d904908279bf90f56be2967e1daf04dce6
kimYSang <qwdbstkd@naver.com>
2018-06-09 13:24:46 +0900

    login & join

--- src/main/java/kr/ac/skuniv/HomeController.java
+++ src/main/java/kr/ac/skuniv/HomeController.java
@@ -29,12 +29,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	PreparedStatement ps = null;
+	ResultSet rs = null;		
+	Connection conn = null;
 	
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public void home(HttpServletRequest request ,HttpServletResponse response)  throws ServletException, IOException {
-		System.out.println("home");
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public void login(HttpServletRequest request ,HttpServletResponse response)  throws ServletException, IOException {
+		System.out.println("login");
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
+		
+		boolean loginCheck = false;	
+		String id ="";
+		String pw="";
 		String name = "";
 
 		//파라미터로 아이디와 비밀번호 받음
@@ -42,33 +49,10 @@ public class HomeController {
 		String parent_pw = (String) request.getParameter("parent_pw");
 
 		System.out.println("id = " + parent_id + "pw = " + parent_pw);
-
+		
+		String sql = "select * from client";
 		try {
-			//로그인 확인하는 메소드 호출
-			name = Login(parent_id,parent_pw);
-		}catch(ClassNotFoundException e) {
-			e.printStackTrace();
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}
-
-		//로그인 성공 실패 여부 확인해서 메세지 보내기
-		if(name.equals("fail"))
-			response.getWriter().print("fail");
-		else response.getWriter().print(name);
-	}
-	
-	public String Login(String parent_id,String parent_pw) throws ClassNotFoundException, SQLException {
-		Connection conn = getConnection();
-		PreparedStatement ps = null;
-		ResultSet rs = null;		
-		boolean loginCheck = false;
-		String id ="";
-		String pw="";
-		String name="";
-		String sql = "select * from logintest";
-		try {
-			System.out.println("mysql 접속중");
+			conn = getConnection();
 			ps = conn.prepareStatement(sql);
 			rs = ps.executeQuery();
 
@@ -83,27 +67,96 @@ public class HomeController {
 					break;
 				}
 			}
-		}catch(Exception e) {
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}catch(SQLException e) {
 			e.printStackTrace();
 		}finally {
 			try {
 				ps.close();
 				conn.close();
-				rs.close();
+				//rs.close();
 				System.out.println("mysql 끝");
 			}catch(SQLException e) {
 				e.printStackTrace();
 			}
 		}
+		
+		System.out.println(name);
+		
 		if(loginCheck)
-			return name;
+			response.getWriter().print(name);
 		else
-			return "fail";
+			response.getWriter().print("fail");
+	}
+	
+	@RequestMapping(value = "/join", method = RequestMethod.POST)
+	public void join(HttpServletRequest request ,HttpServletResponse response)  throws ServletException, IOException {
+		System.out.println("join");
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		
+		String new_id = (String) request.getParameter("new_id");
+		String new_pw = (String) request.getParameter("new_pw");
+		String new_name = (String) request.getParameter("new_name");
+		
+		String id ="";
+		String result="";
+		
+		String sql = "select id from client";
+		try {
+			conn = getConnection();
+			ps = conn.prepareStatement(sql);
+			rs = ps.executeQuery();
+
+			int i = 0;
+			while(rs.next()) {
+				id = rs.getString(1);
+				
+				if(new_id.equals(id)) {
+					result = "overlap";
+					break;
+				}
+			}
+			
+			if(result.equals("")) {
+				sql = "insert into client values ('" + new_id + "','" + new_pw + "','" + new_name + "');";
+				ps = conn.prepareStatement(sql);
+				ps.executeUpdate();
+				result = "success";
+				System.out.println("insert success!!!");
+			}
+			
+			
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				ps.close();
+				conn.close();
+				System.out.println("mysql 끝");
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}		
+		
+		if(result.equals("overlap")) {
+			response.getWriter().print("overlap");
+		}
+		else if(result.equals("success")) {
+			response.getWriter().print("success");
+		}
+		else {
+			response.getWriter().print("fail");
+		}
+		
 	}
 
 	private Connection getConnection() throws ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.jdbc.Driver");
-		Connection c = DriverManager.getConnection("jdbc:mysql://117.17.142.207:3306/hanium", "kimys", "kim111");
+		Connection c = DriverManager.getConnection("jdbc:mysql://117.17.142.207:3306/yesterday", "kimys", "kim111");
 		return c;
 	}
 
